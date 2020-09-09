c25095c7381788f5b8271b2f94052fa19bcd8c95
mattwien <70956195+mattwien@users.noreply.github.com>
2020-09-09 20:02:09 +0200

    Version 0.1

--- mwCiteBox.js
+++ mwCiteBox.js
@@ -26,31 +26,30 @@ $(function() {
     
     var content = getTextSelection();
     var url     = window.location.href; 
-    url = url.toLocaleLowerCase();
+    var url = url.toLocaleLowerCase();
+    var title = "";
+    var author = "";
     // check if on lesen.amazon.de ??? notebook
 
+
     // Amazon Notebook:
-    var title = $('h3.kp-notebook-metadata').text();
-    var author = $('p.a-spacing-top-micro.kp-notebook-metadata').text();
-
-
     if (url.indexOf("amazon.de") >0  & url.indexOf("notebook") >0) {
-            //alert(booktitle);
-        alert("Author: " + author);
-
-    } else {
-        alert("notAmazon");
+        title = $('h3.kp-notebook-metadata').text();
+        author = $('p.a-spacing-top-micro.kp-notebook-metadata').text();
     }
 
     var citeBox = '<div id="mwCiteBox" style="display: block; position: fixed; z-index: 1; padding-top: 100px;left: 0;top: 0;width: 100%;height: 100%;overflow: auto;background-color: rgb(0,0,0);background-color: rgba(0,0,0,0.4);line-height: normal;">'+
                 '<div id="mwCiteBoxContent" style="margin: auto;padding: 20px;border: 1px solid #888;width: 40%;border-radius:5px;-moz-box-shadow:0 0 5px #888;-webkit-box-shadow:0 0 5px#888;box-shadow:0 0 5px #888;background:#eee;">'+
-                    '<div id="mwCiteBoxCite" style="color: #000; font-family: Times New Roman, Times, serif; font-size:20px;">»' + content + '«</div>'+
-                    '<div id="mwCiteBoxTitle" style="font-family: Arial, Helvetica, sans-serif;padding-top:10px;font-size:16px;">' + title + '</div>'+
-                    '<div id="mwCiteBoxAuthor" style="font-family: Arial, Helvetica, sans-serif;padding-top:5px;color:#a49e99;font-size:14px;">' + author + '</div>'+
-                '</div>'+
-            '</div>';
+                    '<div id="mwCiteBoxCite" style="color: #000; font-family: Times New Roman, Times, serif; font-size:20px;">»' + content + '«</div>';
+    if (title!="") {
+        citeBox+= '<div id="mwCiteBoxTitle" style="font-family: Arial, Helvetica, sans-serif;padding-top:10px;font-size:16px;">' + title + '</div>';
+        
+    }
+    if (author="") {    
+        citeBox+='<div id="mwCiteBoxAuthor" style="font-family: Arial, Helvetica, sans-serif;padding-top:5px;color:#a49e99;font-size:14px;">' + author + '</div>';
+    }
+    citeBox +='</div></div>';
 
-    alert(content);
     $('body').prepend(citeBox);
     $('#mwCiteBox').click(function() {
         $("#mwCiteBox").remove();
