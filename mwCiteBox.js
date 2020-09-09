2530d7d69266c9238a62b5470ccb656fa3f33122
mattwien <70956195+mattwien@users.noreply.github.com>
2020-09-09 19:55:12 +0200

    Version 0.1

--- mwCiteBox.js
+++ mwCiteBox.js
@@ -1,6 +1,11 @@
 // can be changed to a bookmarklet with a bookmarklet maker like e.g.:
 // https://bookmarklets.org/maker/
 
+// inspired by :
+// https://www.javascript-kurs.de/javascript-getSelection.htm
+// https://www.w3schools.com/howto/howto_css_modals.asp
+// 
+
 function getTextSelection(){
     var nutzertextauswahl = "";
     if (window.getSelection) { 
@@ -37,9 +42,18 @@ $(function() {
         alert("notAmazon");
     }
 
-    var divBox = '<div style="border:1px solid #888;border-radius:5px;-moz-box-shadow:0 0 5px #888;-webkit-box-shadow:0 0 5px#888;box-shadow:0 0 5px #888;background:#eee;text-align:left;padding:1em;">' + 
-                '<a href="#" onclick="document.body.removeChild(document.body.firstChild);return false">remove</a>' + content + '</div>'
+    var citeBox = '<div id="mwCiteBox" style="display: block; position: fixed; z-index: 1; padding-top: 100px;left: 0;top: 0;width: 100%;height: 100%;overflow: auto;background-color: rgb(0,0,0);background-color: rgba(0,0,0,0.4);line-height: normal;">'+
+                '<div id="mwCiteBoxContent" style="margin: auto;padding: 20px;border: 1px solid #888;width: 40%;border-radius:5px;-moz-box-shadow:0 0 5px #888;-webkit-box-shadow:0 0 5px#888;box-shadow:0 0 5px #888;background:#eee;">'+
+                    '<div id="mwCiteBoxCite" style="color: #000; font-family: Times New Roman, Times, serif; font-size:20px;">»' + content + '«</div>'+
+                    '<div id="mwCiteBoxTitle" style="font-family: Arial, Helvetica, sans-serif;padding-top:10px;font-size:16px;">' + title + '</div>'+
+                    '<div id="mwCiteBoxAuthor" style="font-family: Arial, Helvetica, sans-serif;padding-top:5px;color:#a49e99;font-size:14px;">' + author + '</div>'+
+                '</div>'+
+            '</div>';
+
     alert(content);
-    $('body').prepend(divBox);
+    $('body').prepend(citeBox);
+    $('#mwCiteBox').click(function() {
+        $("#mwCiteBox").remove();
+    });
     
 });
