451c04a3421bcaa25af768a71533a2ca2e287aa3
mattwien <70956195+mattwien@users.noreply.github.com>
2020-09-10 11:51:08 +0200

    First beta version

--- mwCiteBox.js
+++ mwCiteBox.js
@@ -1,4 +1,4 @@
-// can be changed to a bookmarklet with a bookmarklet maker like e.g.:
+ // can be changed to a bookmarklet with a bookmarklet maker like e.g.:
 // https://bookmarklets.org/maker/
 
 // inspired by :
@@ -80,7 +80,7 @@ $(function() {
     url = truncate (url,100,80,4,false);
 
     var citeBox = '<div id="mwCiteBox" style="display: block; position: fixed; z-index: 1; padding-top: 100px;left: 0;top: 0;width: 100%;height: 100%;overflow: auto;background-color: rgb(0,0,0);background-color: rgba(0,0,0,0.4);line-height: normal;">'+
-                '<div id="mwCiteBoxContent" style="margin: auto;padding: 20px;border: 1px solid #888;width: 40%;border-radius:15px;-moz-box-shadow:0 0 15px #888;-webkit-box-shadow:0 0 15px#888;box-shadow:0 0 15px #888;background:#eee;">'+
+                '<div id="mwCiteBoxContent" style="margin: auto;padding: 40px;border: 1px solid #888;width: 40%;border-radius:5px;-moz-box-shadow:0 0 5px #888;-webkit-box-shadow:0 0 5px#888;box-shadow:0 0 5px #888;background:#eee;">'+
                     '<div id="mwCiteBoxCite" style="color: #000; font-family: Times New Roman, Times, serif; font-size:20px;">»' + content + '«</div>';
     if (title!="") {
         citeBox+= '<div id="mwCiteBoxTitle" style="font-family: Arial, Helvetica, sans-serif;padding-top:10px;font-size:16px;">' + title + '</div>';
